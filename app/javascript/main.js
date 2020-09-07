9a733163fb3fcaeba880c1ded71acfc3bd9d385a
EC2 Default User <ec2-user@ip-172-31-68-82.ec2.internal>
2020-09-07 18:46:10 +0000

    Start mobile navbar

---
+++ app/javascript/main.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("mobile-navbar");
+btn.addEventListener("click", handleClick);
+console.log("JS loaded");
+
+function handleClick() {
+  alert("I'm clicked!");
+}
