911e4de02fc8b12d964ef2b82f3ee70fae8c4d0d
EC2 Default User <ec2-user@ip-172-31-68-82.ec2.internal>
2020-09-07 00:46:12 +0000

    Add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title = ''
+    base_title = "BTGA"
+    if page_title
+      page_title + " | " + base_title
+    else
+      base_title
+    end
+  end
 end
