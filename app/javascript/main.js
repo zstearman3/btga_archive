bd5daa54ecdc0b959edc89ca6777e09036f671ed
EC2 Default User <ec2-user@ip-172-31-68-82.ec2.internal>
2020-09-08 02:34:49 +0000

    Add mobile dropdown button

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,6 +1,8 @@
-const btn = document.getElementById("mobile-navbar");
-btn.addEventListener("click", handleClick);
-console.log("JS loaded");
+document.addEventListener("turbolinks:load", () => {
+  const btn = document.getElementById("mobile-navbar");
+  btn.addEventListener("click", handleClick);
+  console.log("JS loaded");
+})
 
 function handleClick() {
   alert("I'm clicked!");
