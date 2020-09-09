6c68c7c31e94bd7643da762e8530a0d67c604aee
EC2 Default User <ec2-user@ip-172-31-68-82.ec2.internal>
2020-09-09 14:37:57 +0000

    Add golfer update and delete

--- app/controllers/golfers_controller.rb
+++ app/controllers/golfers_controller.rb
@@ -1,4 +1,5 @@
 class GolfersController < ApplicationController
+  before_action :select_golfer, only: [:show, :edit, :update, :destroy]
   
   def index
     @golfers = Golfer.all
@@ -21,15 +22,31 @@ class GolfersController < ApplicationController
     end
   end
   
-  def edit
-    
-  end
+  def edit; end
   
   def update
-    
+    if @golfer.update(golfer_params)
+      flash[:success] = "Golfer updated!"
+      redirect_to roster_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @golfer.destroy ? flash[:success] = "Golfer deleted!" : flash[:danger] = "There was a problem deleting the golfer."
+    redirect_to roster_path
   end
   
   private
+    def select_golfer
+      begin
+        @golfer = Golfer.find(params[:id])
+      rescue StandardError => e
+        redirect_to roster_path
+        flash[:danger] = e.message
+      end
+    end
   
     def golfer_params
       params.require(:golfer).permit(:name, :gamertag, :handicap, :victories, :society_id)
--- app/models/golfer.rb
+++ app/models/golfer.rb
@@ -1,4 +1,12 @@
 class Golfer < ApplicationRecord
   belongs_to :society
   validates :name, presence: true
+  
+  def formatted_handicap
+    if handicap < 0
+     "+ #{handicap.abs.to_s}"
+    else
+      handicap.to_s
+    end
+  end
 end
