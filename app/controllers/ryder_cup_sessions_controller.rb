eb4cc9973b104a778960c1a7eba89b51f8188c37
EC2 Default User <ec2-user@ip-172-31-68-82.ec2.internal>
2020-10-26 21:23:23 +0000

    Fix link text

--- app/controllers/ryder_cup_sessions_controller.rb
+++ app/controllers/ryder_cup_sessions_controller.rb
@@ -2,6 +2,7 @@ class RyderCupSessionsController < ApplicationController
   def show
     @session = RyderCupSession.includes(:ryder_cup_rounds).find(params[:id])
     @rounds = @session.ryder_cup_rounds
-    @season = @session.ryder_cup.season
+    @ryder_cup = @session.ryder_cup
+    @season = @ryder_cup.season
   end
 end
