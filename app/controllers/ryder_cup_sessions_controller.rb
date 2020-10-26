3cee7541cc0116cc6e1f7beb762f9d4aa95bad6f
EC2 Default User <ec2-user@ip-172-31-68-82.ec2.internal>
2020-10-26 20:36:26 +0000

    Fix backlink

--- app/controllers/ryder_cup_sessions_controller.rb
+++ app/controllers/ryder_cup_sessions_controller.rb
@@ -2,5 +2,6 @@ class RyderCupSessionsController < ApplicationController
   def show
     @session = RyderCupSession.includes(:ryder_cup_rounds).find(params[:id])
     @rounds = @session.ryder_cup_rounds
+    @season = @session.ryder_cup.season
   end
 end
